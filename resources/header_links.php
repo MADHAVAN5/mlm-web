12ead5b951e5f60ac7befa5dbecce411fd7360b5
MADHAVAN5 <kmadavan018@gmail.com>
2022-11-17 23:26:53 +0530

    dashboard

--- resources/header_links.php
+++ resources/header_links.php
@@ -1,13 +1,10 @@
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>Components / Alerts - NiceAdmin Bootstrap Template</title>
 <meta content="" name="description">
 <meta content="" name="keywords">
 
 <!-- Favicons -->
-<link href="assets/img/favicon.png" rel="icon">
-<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="assets/img/logo_icon.png" rel="icon">
+<link href="assets/img/apple-touch-logo.png" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link href="https://fonts.gstatic.com" rel="preconnect">
